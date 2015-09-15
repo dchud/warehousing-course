3d2ed710b63c0e30e7f94c3a054ddce8e2db24eb
Dan Chudnov <dchud@umich.edu>
2015-09-15 19:07:09 -0400

    added week 3 notebooks

---
+++ lectures/week-03/pyfilter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+A filter that ___.
+"""
+
+import fileinput
+
+def process(line):
+    """For each line of input, ___."""
+    print line
+
+
+for line in fileinput.input():
+    process(line)
